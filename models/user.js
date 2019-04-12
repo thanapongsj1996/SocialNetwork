be540c971cfa4c82b453bfadf5e31353ba801c9c
Thanapong <thanapongsj1996@gmail.com>
2019-04-12 18:17:08 +0700

    Signin and generate token

--- models/user.js
+++ models/user.js
@@ -41,6 +41,9 @@ userSchema.virtual('password')
 
 // method
 userSchema.methods = {
+    authenticate: function(plainText){
+        return this.encryptPassword(plainText) === this.hashed_password
+    },
     encryptPassword: function (password) {
         if (!password) return ''
         try {

76bf06ad21e4c9ea61486ef406be4a331b07d8c0
Thanapong <thanapongsj1996@gmail.com>
2019-04-16 01:03:04 +0700

    showing single user

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const { userById, allUsers } = require('../controllers/user')
+const { userById, allUsers, getUser } = require('../controllers/user')
+const { requireSignin } = require('../controllers/auth')
 
 const router = express.Router()
 
-router.get('/users', allUsers)
+router.get('/users', requireSignin, allUsers)
+router.get('/user/:userId', requireSignin, getUser)
 
 // any route containing : userId, our app will first execute userById()
 router.param('userId', userById)

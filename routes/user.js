68d02eaff40531d8f160b2375f3ee25af36b28b3
Thanapong <thanapongsj1996@gmail.com>
2019-04-16 01:30:46 +0700

    Update user information

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 const express = require('express')
-const { userById, allUsers, getUser } = require('../controllers/user')
+const { userById, allUsers, getUser, updateUser } = require('../controllers/user')
 const { requireSignin } = require('../controllers/auth')
 
 const router = express.Router()
 
 router.get('/users', requireSignin, allUsers)
 router.get('/user/:userId', requireSignin, getUser)
+router.put('/user/:userId', requireSignin, updateUser)
 
 // any route containing : userId, our app will first execute userById()
 router.param('userId', userById)
